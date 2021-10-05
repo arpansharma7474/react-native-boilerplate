10cd1c903cb69a5d0038b2d9aeee1ac56a8326e4
sudhanshu joshi <joshisudhanshu24@gmail.com>
2021-10-05 08:56:36 +0530

    Fixes-Fixed issue in AppDelegate file

--- ios/RNBoilerPlate/AppDelegate.m
+++ ios/RNBoilerPlate/AppDelegate.m
@@ -28,7 +28,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
- 
+   [FIRApp configure];
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
